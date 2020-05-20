454fb1e664b2885ff4ad4fdccd45ed9d91dc8604
Zachary Allen <zachary.thomas.allen@outlook.com>
2020-05-20 00:21:29 -0400

    Add project files.

---
+++ MVC-Tutorial/Models/Enrollment.cs
@@ -0,0 +1,31 @@
+﻿namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {//THESE PROPERTIES ARE PASSED INTO STUDENT.CS WHEN ICOLLECTION<ENUMERABLE> IS CALLED
+
+
+        //This file connects to the user side to take in data and move it to the Student class to be deciphered and stored in the database. Technically the data that is stored here is like cache because it doesn't go deeper and can be reached faster.
+
+        //[Database Properties]
+        public int EnrollmentID { get; set; }
+            //[Primary Key]
+
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+            //[Foreign Key]
+
+        public Grade? Grade { get; set; }
+            //[The grade property is an enum (as listed at the top of the file)]
+            //The '?' means that the value can be null, null is different in '0' as it is not addes in the calculations.  
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+
+        //[End of Database Properties]
+    }
+}
---
+++ MVC-Tutorial/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        // THIS CLASS OF PROPERTIES IS CALLED WHEN THE STUDENT CLASS IS CALLED
+
+        //[enrollment properties]
+        public int ID { get; set; }
+            //[Primary Key]
+            //default is an ID of some form. i.e. [***ID]
+
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        //THE PROPERTIES IN THE ENROLLEMNT CLASS IS CALLED HERE.
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
